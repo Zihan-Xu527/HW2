f90fc2e0268120034b4fa803e0afefc9a1304992
Zihan-Xu527 <zxu64@ucmerced.edu>
2022-10-11 14:52:05 -0700

    add pdf

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+//#include "Grid2d.h"
+
+// using namespace std;  < -- Streamline?
+
+int main() {
+    std::cout << "Hello, MATH233! :)" << std::endl;
+
+
+    return 0;
+
+}
+
